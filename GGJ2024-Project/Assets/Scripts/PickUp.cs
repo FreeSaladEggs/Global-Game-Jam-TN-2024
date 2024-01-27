6e6d8f1419f85514ba87c7299d79b09c16171594
FreeSaladEggs <meychlibi@gmail.com>
2024-01-27 04:30:28 +0100

    Picking system done, now doing dropping

--- GGJ2024-Project/Assets/Scripts/PickUp.cs
+++ GGJ2024-Project/Assets/Scripts/PickUp.cs
@@ -1,25 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    float pickUpDistance;
+    float pickUpDistance = 2f;
+    Ray ray; 
     Rigidbody rb;
+    public GameObject pickableObject ;
+    public Transform virtualHand;
+    public bool hasPickedObject = false;
+    
 
     // Start is called before the first frame update
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>(); 
-    }
     void Start()
     {
+       
         
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit , pickUpDistance) && Input.GetKey(KeyCode.G) && (!hasPickedObject))
+        {
+            Pickup();
+        }
     }
+
+    private void Pickup()
+    {
+        pickableObject.GetComponent<Rigidbody>().isKinematic = true;
+        hasPickedObject = true;
+        pickableObject.transform.SetParent(virtualHand);
+        pickableObject.transform.localPosition = Vector3.zero;
+
+
+
+    }
+   
 }
