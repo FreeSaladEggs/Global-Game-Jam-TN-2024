a66473117a0415de0939287f536d5cedc1e72239
Khalil Chabir <khalil.chabir96@gmail.com>
2024-01-28 03:25:49 +0100

    aim sayer

--- GGJ2024-Project/Assets/Scripts/PickUp.cs
+++ GGJ2024-Project/Assets/Scripts/PickUp.cs
@@ -3,34 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class PickUp : MonoBehaviour
 {
-    float pickUpDistance = 2f;
+    public float pickUpDistance = 2f;
     Ray ray;
     public LayerMask Pickable;
     GameObject pickableObject;
     public Transform virtualHand;
     public bool hasPickedObject = false;
 
+    public GameObject test;
+
+    [SerializeField] private Camera cam;
+
+
     // Update is called once per frame
     void Update()
     {
-        ray = new Ray(transform.position, transform.forward);
-        RaycastHit[] hits = Physics.RaycastAll(ray, pickUpDistance, Pickable);
+        /*ray = new Ray(transform.position, transform.forward);*/
+        ray = new Ray(test.transform.position, test.transform.forward);
+        RaycastHit[] hits = Physics.RaycastAll(ray, pickUpDistance, Pickable) ;
 
 
         if (!hasPickedObject && Input.GetKey(KeyCode.G))
         {
+            Debug.Log("It works 1");
             foreach (RaycastHit hit in hits)
             {
                 pickableObject = hit.collider.gameObject;
                 if (pickableObject.layer == 6)
                 {
-                    Debug.Log("hezz object: " + pickableObject.name);
+                    Debug.Log("It works 2");
                     Pickup();
                 }
             }
+
+
+            /*  pickableObject = hit.collider.gameObject;
+              if (pickableObject.layer == 6)
+              {
+                  Debug.Log("It works 2");
+                  Pickup();
+              }
+          Physics.Raycast(cam.transform.position, cam.transform.forward, out var hit, pickUpDistance)*/
+
+
         }
         if (hasPickedObject && (Input.GetKeyDown(KeyCode.V)))
         {
